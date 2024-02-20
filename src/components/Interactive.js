7a8579661f688525c1c3d2ed1055be822c2be757
Ed Nawrocki <eddnawrocki@gmail.com>
2024-02-19 21:18:27 -0800

    Interactive and Big Truck!

---
+++ src/components/Interactive.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import title from '../images/Interactive.png';
+import pic from '../images/2021-Chevrolet-Silverado-Yenko-SC-front-three-quarter.png';
+import { mediaQueries } from '../shared/config'
+import gradient from '../images/Gradient.png'
+
+const BG = styled('div')`
+    background-color: #F8EA41;
+    height: 898px;
+    border-color: black;
+    border-top-style: solid;
+    border-bottom-style: solid;
+    position: relative;
+    text-align: center;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ImageContainer = styled.div`
+    background-color: #F7EDCC;
+    width: 100%;
+    z-index: -1;
+    ${mediaQueries.mobile}{
+        border-width: 12px;
+    }
+`;
+
+const Title = styled.img`
+    top: 15px;
+    position: relative;
+    z-index: 5; 
+    ${mediaQueries.mobile}{
+        width: 339px;
+    }
+`;
+
+const Interactive = (props) => {
+    return (
+        <>
+        <ImageContainer>
+                <Title src={title} />
+        </ImageContainer>
+        <BG>
+                <img src={pic}></img>
+        </BG>
+        <BG>
+            <img src={props.interactiveIMG}></img>
+        </BG>
+        </>
+
+    );
+};
+
+export default Interactive;
